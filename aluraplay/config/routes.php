28992b91cd0c749d0a7d218fe172b0122c1b06ed
Luiz Augusto <n0cturn0.debian@gmail.com>
2023-04-21 16:34:54 -0400

    Projeto Completo Finalizado

---
+++ aluraplay/config/routes.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'GET|/' => \Alura\Mvc\Controller\VideoListController::class,
+    'GET|/novo-video' => \Alura\Mvc\Controller\VideoFormController::class,
+    'POST|/novo-video' => \Alura\Mvc\Controller\NewVideoController::class,
+    'GET|/editar-video' => \Alura\Mvc\Controller\VideoFormController::class,
+    'POST|/editar-video' => \Alura\Mvc\Controller\EditVideoController::class,
+    'GET|/remover-video' => \Alura\Mvc\Controller\DeleteVideoController::class,
+    'GET|/login' => \Alura\Mvc\Controller\LoginFormController::class,
+    'POST|/login' => \Alura\Mvc\Controller\LoginController::class,
+    'GET|/logout' => \Alura\Mvc\Controller\LogoutController::class,
+    'GET|/videos-json' => \Alura\Mvc\Controller\JsonVideoListController::class,
+    'POST|/videos' => \Alura\Mvc\Controller\NewJsonVideoController::class,
+];
