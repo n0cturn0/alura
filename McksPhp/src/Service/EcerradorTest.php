768cbb7364c0aad0ae201a78be55247367e31339
Luiz Auguto <loliveira@inovvati.com.br>
2023-07-03 19:35:38 -0400

    Criada entrada de Leilão de dois onejtos Fiat e Variant

--- McksPhp/src/Service/EcerradorTest.php
+++ McksPhp/src/Service/EcerradorTest.php
@@ -1,11 +1,21 @@
 <?php 
 
 namespace Alura\Leilao\Service;
+use Alura\Leilao\Model\Leilao;
+use Alura\Leilao\Service\Encerrador;
 use PHPUnit\Framework\TestCase;
 class EncerradorTest extends TestCase
 {
     public function testLeioloesComMaisDeUmaSemanaDeveSerEncerrados() 
     {
+        $fia147 = new Leilao(
+          'Fiat 147 0Km',
+          new \DateTimeImmutable('8 days ago') 
+        );
+        $variant = new Leilao(
+            'Variant 1972 0Km',
+            new \DateTimeImmutable('10 days ago') 
+          );
         $encerrador = new Encerrador();
         $encerrador->encerra();
 
